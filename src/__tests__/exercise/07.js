0dfd1a75cb2bd4c302af255b9ea7de0ec106dd67
ra-md <ra799m@gmail.com>
2021-08-27 15:21:11 +0700

    7

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -2,18 +2,47 @@
 // http://localhost:3000/easy-button
 
 import * as React from 'react'
-import {render, screen} from '@testing-library/react'
+import {screen} from '@testing-library/react'
+import {render} from '../../test/test-utils'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
+// function renderWithThemeProvider(ui, theme = 'light') {
+//   const Wrapper = ({children}) => {
+//     return <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
+//   }
+//   render(ui, {wrapper: Wrapper})
+// }
+
 test('renders with the light styles for the light theme', () => {
   // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
+  // const Wrapper = ({children}) => {
+  //   return <ThemeProvider initialTheme='light'>{children}</ThemeProvider>
+  // }
+  // renderWithThemeProvider(<EasyButton>Easy</EasyButton>)
+  render(<EasyButton>Easy</EasyButton>)
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `)
+  //
+  // 🐨 update the `render` call above to use the wrapper option using the
+  // ThemeProvider
+})
+
+test('renders with the dark styles for the dark theme', () => {
+  // 🐨 uncomment all of this code and your test will be busted on the next line:
+  // const Wrapper = ({children}) => {
+  //   return <ThemeProvider initialTheme='dark'>{children}</ThemeProvider>
+  // }
+  // renderWithThemeProvider(<EasyButton>Easy</EasyButton>, 'dark')
+  render(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `)
   //
   // 🐨 update the `render` call above to use the wrapper option using the
   // ThemeProvider
